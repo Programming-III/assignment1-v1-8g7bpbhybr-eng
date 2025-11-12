a25d6918440cf9c586379d4f60a692279bd45752
malak ehab <8g7bpbhybr@privaterelay.appleid.com>
2025-11-12 15:17:41 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,29 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
-
+#ifndef Enclosure_H
+#define Enclosure_H
+#include <iostream>
+#include <string>
+using namespace std;
 #include "Animal.h"
 
-//define enclosure class here 
+class Enclosure {
+private:
+Animal** animals;
+int capacity;
+int currentCount;
 
+public:
+   
+Enclosure();
+Enclosure(int capacity);
+~Enclosure();
 
+int getCapacity() const;
+void setCapacity(int capacity);
+int getCurrentCount() const;
 
+   
+    void addAnimal(Animal* a);
+    void displayAnimals();
+};
 
-
-
-
-
-#endif
+#endif Enclosure_H
